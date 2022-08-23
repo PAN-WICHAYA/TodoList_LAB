8723e0614b1e9fdb85996d869139c8d0ea5ef1f5
PAN-WICHAYA <wichaya@automationfirst.co.th>
2022-08-23 11:06:02 +0700

    fixed component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import TodoContainer from './component/TodoContainer';
 import TodoForm from './component/TodoForm';
-import Pagination from './component/Pagination';
-import TodoList from './component/TodoList';
 function App() {
   return (
     <div className='container mt-5 mb-3' style={{ maxWidth: 576 }}>
@@ -12,25 +10,6 @@ function App() {
       </div>
 
       <TodoContainer />
-      <TodoList />
-      {/* ********** SEARCH TEXT ********** */}
-
-      {/* ********** END SEARCH TEXT ********** */}
-
-      {/* ********** SEARCH STATUS ********** */}
-
-      {/* ********** END SEARCH STATUS ********** */}
-
-      {/* ********** TODO LIST ********** */}
-
-      {/* ********** END TODO LIST ********** */}
-
-      <div className='my-2 d-flex justify-content-between align-items-center'>
-        <small className='text-muted'>Showing 6 to 10 of 12 entries</small>
-        {/* ********** PAGINATION ********** */}
-        <Pagination />
-        {/* ********** END PAGINATION ********** */}
-      </div>
     </div>
   );
 }
--- src/component/TodoContainer.js
+++ src/component/TodoContainer.js
@@ -3,6 +3,8 @@ import SearchStatus from './SearchStatus';
 import SearchText from './SearchText';
 import PageLimit from './PageLimit';
 import Sort from './Sort';
+import TodoList from './TodoList';
+import Pagination from './Pagination';
 
 function TodoContainer() {
   return (
@@ -15,6 +17,25 @@ function TodoContainer() {
       <div className='my-2 d-flex justify-content-between'>
         <PageLimit />
         <Sort />
+      </div>
+      <TodoList />
+      {/* ********** SEARCH TEXT ********** */}
+
+      {/* ********** END SEARCH TEXT ********** */}
+
+      {/* ********** SEARCH STATUS ********** */}
+
+      {/* ********** END SEARCH STATUS ********** */}
+
+      {/* ********** TODO LIST ********** */}
+
+      {/* ********** END TODO LIST ********** */}
+
+      <div className='my-2 d-flex justify-content-between align-items-center'>
+        <small className='text-muted'>Showing 6 to 10 of 12 entries</small>
+        {/* ********** PAGINATION ********** */}
+        <Pagination />
+        {/* ********** END PAGINATION ********** */}
       </div>
     </Fragment>
   );
